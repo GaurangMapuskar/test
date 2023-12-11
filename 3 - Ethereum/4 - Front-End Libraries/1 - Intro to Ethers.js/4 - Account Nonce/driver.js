e272f6794fc6f341aec8c015dab54870491c2436
Gaurang M <gaurangmapuskar@gmail.com>
2023-12-11 09:27:36 +0530

    Changes in the driver code

--- 3 - Ethereum/4 - Front-End Libraries/1 - Intro to Ethers.js/4 - Account Nonce/driver.js
+++ 3 - Ethereum/4 - Front-End Libraries/1 - Intro to Ethers.js/4 - Account Nonce/driver.js
@@ -3,18 +3,12 @@ const ethers = require('ethers');
 // Driver code to send Ether
 async function main() {
     try {
-        // Specify the recipient address and the amount of Ether to send
-        const recipientAddress = '0xdD0DC6FB59E100ee4fA9900c2088053bBe14DE92';
+        // Specify the recipient address and the amount of Ether to send 0xdD0DC6FB59E100ee4fA9900c2088053bBe14DE92
+        const recipientAddress = '';
         const etherAmount = ethers.utils.parseUnits('1', 'ether');
-
-        // Call the sendEther function with the transaction details
-        const transaction = await sendEther({
-            value: etherAmount,
-            to: recipientAddress,
-        });
-
-        // Log the transaction hash
-        console.log('Transaction Hash:', transaction.hash);
+         
+        // Create a Transaction and send it.
+        
     } catch (error) {
         console.error('Error sending Ether:', error.message);
     }
